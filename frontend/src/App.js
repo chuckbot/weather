3525bf8ab2d472cffd54c5878dd7cbb51e2280ef
chuckbot <cgmilan@gmail.com>
2022-06-11 11:44:47 -0400

    React setup

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import axios from "axios";
+
+function App() {
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=10.500000&lon=-66.916664&appid=26d12df405e45a6e2a2e4923b11c74d2`
+
+  return <div className="App">
+    <h1>Hello</h1>
+  </div>;
+}
+
+export default App;
